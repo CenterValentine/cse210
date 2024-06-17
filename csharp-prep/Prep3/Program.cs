3ca7656f2dd996f5c405865ff6e60808da911ac6
CenterValentine <david@doxy.me>
2024-06-16 21:48:48 -0600

    prep3 guess magic number game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string play_again = "yes";
+        while (play_again == "yes") {
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        // Console.WriteLine($"Magic Number: {magicNumber}");
+
+        // Console.WriteLine("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("I'm thinking of a number!");
+        
+
+
+        Console.WriteLine("What do you think the magic number is? ");
+        int guessNumber = int.Parse(Console.ReadLine());
+
+        do
+        {
+        
+        
+
+        if (guessNumber > magicNumber)
+        {
+            Console.WriteLine("Lower");
+            Console.WriteLine("Guess again!");
+            guessNumber = int.Parse(Console.ReadLine());
+        }
+        else if (guessNumber < magicNumber)
+        {
+            Console.WriteLine("Higher");
+            Console.WriteLine("Guess again!");
+            guessNumber = int.Parse(Console.ReadLine());
+        }
+        } while (guessNumber != magicNumber );
+
+        Console.WriteLine($"You guessed it!  The magic number was {magicNumber}!");
+        Console.WriteLine("Would you like to play again?");
+        play_again = Console.ReadLine();
+
+        }
+        Console.WriteLine("Thanks for playing!");
+
+
     }
 }
