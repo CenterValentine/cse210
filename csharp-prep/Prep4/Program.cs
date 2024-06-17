cca0d9189dcd3b448cdfc7f54abe5c9676a571d8
CenterValentine <david@doxy.me>
2024-06-16 22:10:45 -0600

    Prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,49 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+
+        List<int> listOfNumbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int userNumber = -1;
+
+        while (userNumber != 0)
+        {
+            Console.WriteLine("Enter Number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber == 0)
+            {
+                break;
+            };
+            listOfNumbers.Add(userNumber);
+        }
+
+        // Sum and Average
+        int listOfNumbersSum = 0;
+
+        foreach (int number in listOfNumbers)
+        {
+            listOfNumbersSum = listOfNumbersSum + number;
+        }
+
+        int listOfNumbersAvg = listOfNumbersSum / listOfNumbers.Count;
+
+
+        // Average number
+        int maxNumber = listOfNumbers[0];
+
+        foreach (int number in listOfNumbers)
+        {
+            if (number > maxNumber)
+            {
+                maxNumber = number;
+            }
+        }
+        Console.WriteLine($"The sum is: {listOfNumbersSum}");
+        Console.WriteLine($"The average is: {listOfNumbersAvg}");
+        Console.WriteLine($"The max number is: {maxNumber}");
+        // sort the list
+        // List<int> SortedList = listOfNumbers.OrderBy(number => number).ToList();
+        // Console.WriteLine($"The sorted list is: {SortedList}");
     }
 }
